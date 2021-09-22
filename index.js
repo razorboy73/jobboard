7b3b243c648b5139c9c2ad63e27d53c2c84abfeb
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-09-22 14:18:10 -0400

    added styling

--- index.js
+++ index.js
@@ -10,12 +10,32 @@ xhttp.onreadystatechange = function () {
     data.slice(1).forEach(function (element, i) {
       console.log(element, i);
       let elemDiv = document.createElement("div");
-      elemDiv.innerHTML = `
-        <h5 class="company">${element.company}</h5>
-        <h3 class="position">${element.position}</h3>
-        <p class="location">${element.location}</p>
-      `;
+
       elemDiv.classList.add("job");
+
+      elemDiv.innerHTML = `
+        <div class="left-section">
+            <h5 class="company">${element.company}</h5>
+            <h3 class="position">${element.position}</h3>
+            <p class="location">${element.location}</p>
+        </div>
+        <div class="middle-section">
+            ${element.tags
+              .map(function (tag) {
+                return `<div class="tag">${tag}</div>`;
+              })
+              .join(" ")}
+        </div>
+        <div class="mid-right-section">
+        ${new Date(element.date).toLocaleString()}
+        </div>
+        <div class="right-section">
+            <a href="${element.apply_url}">
+                <button class="apply">Apply</button>
+            </a>
+        </div>
+      `;
+
       container.appendChild(elemDiv);
     });
   }
