e0696a387c0e5b622f292cf8d8c440fdb5a9d44d
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-09-22 12:57:40 -0400

    parsed out api

--- index.js
+++ index.js
@@ -5,7 +5,19 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("container").innerHTML = this.responseText;
+    var data = JSON.parse(xhttp.responseText);
+    var container = document.getElementById("container");
+    data.slice(1).forEach(function (element, i) {
+      console.log(element, i);
+      let elemDiv = document.createElement("div");
+      elemDiv.innerHTML = `
+        <h5 class="company">${element.company}</h5>
+        <h3 class="position">${element.position}</h3>
+        <p class="location">${element.location}</p>
+      `;
+      elemDiv.classList.add("job");
+      container.appendChild(elemDiv);
+    });
   }
 };
 xhttp.open("GET", "https://remoteok.io/api", true);
